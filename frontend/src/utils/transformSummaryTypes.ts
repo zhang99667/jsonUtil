454a40e568da6fd6af88463593004eb8dca8007a
zhangzhen40 <zhangzhen40@baidu.com>
2026-07-03 05:01:59 +0800

    [Refactor]收敛报告视图类型职责

--- frontend/src/utils/transformSummaryTypes.ts
+++ frontend/src/utils/transformSummaryTypes.ts
@@ -36,52 +36,15 @@ export interface TransformContextReport {
   runtimePlaceholders: TransformReportRuntimePlaceholder[];
 }
 
-export interface TransformReportView {
-  records: TransformReportRecord[];
-  cmdStructureRecords: TransformReportRecord[];
-  warnings: TransformReportWarning[];
-  unresolvedCandidates: TransformReportUnresolvedCandidate[];
-  runtimePlaceholderGroups: TransformReportRuntimePlaceholderGroup[];
-  runtimePlaceholders: TransformReportRuntimePlaceholder[];
-  filteredRecordCount: number;
-  filteredWarningCount: number;
-  filteredUnresolvedCount: number;
-  filteredPlaceholderCount: number;
-  filteredSchemeParamStageCount: number;
-  filteredSchemeParamStageRepairHintCount: number;
-  filteredNonReversibleParamStageCount: number;
-  filteredCmdStructureCount: number;
-  filteredNestedCommandFieldCount: number;
-  filteredNestedResourceFieldCount: number;
-  totalRecordCount: number;
-  totalWarningCount: number;
-  totalUnresolvedCount: number;
-  totalPlaceholderCount: number;
-  totalSchemeParamStageCount: number;
-  totalSchemeParamStageRepairHintCount: number;
-  totalNonReversibleParamStageCount: number;
-  totalCmdStructureCount: number;
-  totalNestedCommandFieldCount: number;
-  totalNestedResourceFieldCount: number;
-  isRecordTruncated: boolean;
-  isCmdStructureTruncated: boolean;
-  isWarningTruncated: boolean;
-  isUnresolvedTruncated: boolean;
-  isPlaceholderTruncated: boolean;
-}
-
-export interface TransformReportViewOptions {
-  recordLimit?: number;
-  warningLimit?: number;
-  unresolvedLimit?: number;
-  placeholderLimit?: number;
-  cmdStructureLimit?: number;
-}
-
 export type {
   TransformReportCommandSchemaRow,
   TransformReportDecodedPath,
 } from './transformSummaryDecodedPathTypes';
+
+export type {
+  TransformReportView,
+  TransformReportViewOptions,
+} from './transformSummaryViewTypes';
 
 export type {
   TransformReportRecord,
